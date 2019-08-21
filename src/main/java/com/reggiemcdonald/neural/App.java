02cf91c6a50b1adca74b63cb943f040c5507a387
reggiemcdonald <regmcdonald95@gmail.com>
2019-08-20 17:47:40 -0700

    updated App

--- src/main/java/com/reggiemcdonald/neural/App.java
+++ src/main/java/com/reggiemcdonald/neural/App.java
@@ -1,7 +1,5 @@
 package com.reggiemcdonald.neural;
 
-import com.reggiemcdonald.neural.net.Network;
-
 /**
  * Hello world!
  *
@@ -9,9 +7,6 @@ import com.reggiemcdonald.neural.net.Network;
 public class App {
     public static void main( String[] args )
     {
-        Network n = new Network (new int[]{2,3,1});
-        System.out.println(n.getBiases());
-        System.out.println("Weight:");
-        System.out.println(n.getWeights().get(1));
+
     }
 }
